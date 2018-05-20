a85c4f903d0f0d0247c32305a252d021a0b48756
Samuel Kurtzer <samuelk373@gmail.com>
2018-05-20 21:36:50 +0930

    adding a basic setup script

---
+++ basicSetup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/Cartographer373/bashrc
+mv bashrc/* ~
+rm -rf bashrc
+
+git clone https://github.com/Cartographer373/neovim
+mkdir .config/nvim
+mv neovim/* .config/nvim
